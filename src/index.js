0a49ee79ace822cfcab39561f2953baff66e5f47
Jordan Wu <jordan.spencer.wu@gmail.com>
2021-06-12 18:30:38 -0600

    add to setup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
+import "tailwindcss/tailwind.css";
+
 function component() {
-  const element = document.createElement('div');
+  const element = document.createElement("div");
 
   // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = "HELLO WORLD"
+  element.innerHTML = "HELLO JORDAN";
+  element.classList.add("flex");
 
   return element;
 }
 
 document.body.appendChild(component());
-console.log("WORKING?")
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,39 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
+  mode: "development",
+  entry: "./src/index.js",
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+    port: 4000,
+    open: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Web Client",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
 };
